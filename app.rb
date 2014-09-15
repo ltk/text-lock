6df9d30ae32b2360e61253f0f13bd6d27dff1bc6
Lawson Kurtz <lawson.kurtz@viget.com>
2014-09-14 20:25:54 -0600

    Print params

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require 'sinatra'
 require 'typhoeus'
 
 post '/sms' do
+  puts "***** Params *****"
+  puts params.inspect
+
   url = "https://fusion.helium.io/api/759459be-c3a4-4c41-9998-36b812a40272/lock/toggle?token=valid"
   Typhoeus.get(url)
 end
